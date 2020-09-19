41edb009300b60ac8f9c2eee7ffdae0907dd2133
Aseka03 <paviz@rocketfirm.com>
2020-09-19 15:08:53 +0500

    react-book

---
+++ src/components/BookBlock/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+
+function PizzaBlock({ id, name,author, imageUrl, price, onClickAddPizza, addedCount }) {
+
+  const onAddPizza = () => {
+    const obj = {
+      id,
+      name,
+      author,
+      imageUrl,
+      price,
+    };
+    onClickAddPizza(obj);
+  };
+
+  return (
+    <div className="books-block">
+      <img className="books-block__image" src={imageUrl} alt="books" />
+      <h4 className="books-block__title">{name}</h4>
+      <h4 className="books-block__title">{author}</h4>
+      <div className="books-block__bottom">
+        <div className="books-block__price">{price} тг</div>
+        <Button onClick={onAddPizza} className="button--add" outline>
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+              fill="white"
+            />
+          </svg>
+          <span>Добавить</span>
+          {addedCount && <i>{addedCount}</i>}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+PizzaBlock.propTypes = {
+  name: PropTypes.string,
+  imageUrl: PropTypes.string,
+  price: PropTypes.number,
+  onClickAddPizza: PropTypes.func,
+  addedCount: PropTypes.number,
+};
+
+PizzaBlock.defaultProps = {
+  name: '---',
+  price: 0,
+};
+
+export default PizzaBlock;
---
+++ src/pages/Basket.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Basket() {
+  return(
+    <h2>Basket</h2>
+  )
+}
+export default Basket
